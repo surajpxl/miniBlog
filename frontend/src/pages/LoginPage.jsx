8ef599c2b537712dd0b0b620b941d0f30c0a6726
surajpxl <guptasuraj4455@gmail.com>
2025-09-15 23:01:01 +0530

    adding toast on successfully signup

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { loginUser } from "../api";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
